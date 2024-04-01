db2cc918629bfb7bb33162b29ed3210383f9122e
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-04-01 22:38:56 +0100

    feat: next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from '@/components/profile';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <main>
-        <Button> Click mee</Button>
+      <form action={actions.signIn}>
+        <Button type='submit'>
+          Sign in with Github
+        </Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type='submit'>
+          Sign out
+        </Button>
+      </form>
+
+      {
+        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div> Signed out</div>
+      }
+      <Profile />
     </main>
   );
 }
---
+++ src/components/profile.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useSession } from "next-auth/react";
+
+export default function Profile() {
+    const session = useSession();
+
+    if(session.data?.user){
+        return <div> From client: User is signed in</div>;
+    }
+
+    return (  
+        <div>
+            From client: User is not signed in
+        </div>
+    );
+}
