188c24e1a9e862c531ed99c1c3c13e2a2bf62b0f
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-04-02 17:13:15 +0100

    feat: Header and Header auth components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-
 export default async function Home() {
-  const session = await auth();
   return (
-    <main>
-      <form action={actions.signIn}>
-        <Button type='submit'>
-          Sign in with Github
-        </Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type='submit'>
-          Sign out
-        </Button>
-      </form>
-
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div> Signed out</div>
-      }
-      <Profile />
-    </main>
+    <main></main>
   );
 }
