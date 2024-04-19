f2ab9c31fcb8839829224d6b0700149d7c8c2aa4
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-04-19 21:33:12 +0100

    wip

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
-import { Suspense } from "react";
-import PostShow from "@/components/posts/post-show";
-import CommentList from "@/components/comments/comment-list";
-import CommentCreateForm from "@/components/comments/comment-create-form";
-import paths from "@/paths";
-import { fetchCommentsByPostId } from "@/db/queries/comments";
-import PostShowLoading from "../post-show-loading";
+import Link from 'next/link';
+import { Suspense } from 'react';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
+import PostShowLoading from '../post-show-loading';
 
 interface PostShowPageProps {
   params: {
@@ -18,12 +18,12 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
   const { slug, postId } = params;
 
   return (
-    <div className="space-y-3">
-      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {"< "}Back to {slug}
+    <div className='space-y-3'>
+      <Link className='underline decoration-solid' href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
       </Link>
       <Suspense fallback={<PostShowLoading />}>
-        <PostShow postId={postId}/>
+        <PostShow postId={postId} />
       </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -1,23 +1,33 @@
-import { notFound } from "next/navigation";
-import { db } from "@/db";
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+import { Button } from '@nextui-org/react';
+import DeleteIcon from '../common/delete-icon';
 
 interface PostShowProps {
-  postId: string,
+  postId: string;
 }
 
 export default async function PostShow({ postId }: PostShowProps) {
   const post = await db.post.findFirst({
-    where: { id: postId }
-  })
+    where: { id: postId },
+  });
 
   if (!post) {
     notFound();
   }
 
   return (
-    <div className="m-4">
-      <h1 className="text-2xl font-bold my-2">{post.title}</h1>
-      <p className="p-4 border rounded">{post.content}</p>
+    <div className='m-4'>
+      <h1 className='text-2xl font-bold my-2'>{post.title}</h1>
+      <p className='p-4 border rounded'>{post.content}</p>
+      <Button
+        color='danger'
+        variant='bordered'
+        size='sm'
+        startContent={<DeleteIcon />}
+      >
+        Delete user
+      </Button>
     </div>
   );
 }
