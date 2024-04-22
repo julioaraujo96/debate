d99b656d29246adba13cda07145f67a34970c93e
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-04-22 23:40:22 +0100

    feat: confirmation modal

--- src/components/posts/delete-post-form.tsx
+++ src/components/posts/delete-post-form.tsx
@@ -1,27 +1,40 @@
 'use client';
 
-import { useFormState } from 'react-dom';
-import DeleteButton from '../common/delete-button';
-import * as actions from '@/actions';
-import { useRouter } from 'next/router';
+import { useFormState } from "react-dom";
+import DeleteButton from "../common/delete-form-button";
+import * as actions from "@/actions";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { useState } from "react";
 
 interface DeletePostFormProps {
   postId: string;
   previousUrl: string;
 }
 
-export default function DeletePostForm({
-  postId,
-  previousUrl,
-}: DeletePostFormProps) {
-  const [formState, action] = useFormState(
-    actions.deletePost.bind(null, postId, previousUrl),
-    { errors: {} }
-  );
+export default function DeletePostForm({ postId, previousUrl }: DeletePostFormProps) {
 
-  return (
-    <form className='flex justify-end w-full my-2' action={action}>
-      <DeleteButton>Delete Post</DeleteButton>
-    </form>
-  );
+    const [isOpen, setOpen] = useState(false);
+
+    const [formState, action] = useFormState(actions.deletePost.bind(null, postId, previousUrl), { errors: {} })
+
+    return <div >
+        <Button onClick={() => setOpen(!isOpen)}>
+            Delete
+        </Button>
+        <Modal isOpen={isOpen}>
+            <ModalHeader>
+
+            </ModalHeader>
+            <ModalBody>
+                <p>Are you sure you want to delete this post?</p>
+            </ModalBody>
+            <ModalFooter>
+                <form action={action}>
+                    <DeleteButton>
+                        Delete Post
+                    </DeleteButton>
+                </form>
+            </ModalFooter>
+        </Modal>
+    </div>
 }
