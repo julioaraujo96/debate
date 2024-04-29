38a44717775bfc0aef1ec6edf419db5d803802c6
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-04-29 23:27:40 +0100

    feat: replace whitespaces

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -48,7 +48,7 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
     try {
         topic = await db.topic.create({
             data: {
-                slug: result.data.name.replace(' ', ''),
+                slug: result.data.name.replace(/\s/g,''),
                 description: result.data.description,
             }
         })
