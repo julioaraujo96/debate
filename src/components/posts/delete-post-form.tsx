0f58a834e3c08be6e6e7822988367dcd6f1722d0
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-05-02 23:36:13 +0100

    feat: change delete button color

--- src/components/posts/delete-post-form.tsx
+++ src/components/posts/delete-post-form.tsx
@@ -1,50 +1,60 @@
 'use client';
 
-import { useFormState } from "react-dom";
-import DeleteFormButton from "../common/delete-form-button";
-import * as actions from "@/actions";
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
-import { useState } from "react";
-import DeleteIcon from "../common/delete-icon";
+import { useFormState } from 'react-dom';
+import DeleteFormButton from '../common/delete-form-button';
+import * as actions from '@/actions';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from '@nextui-org/react';
+import { useState } from 'react';
+import DeleteIcon from '../common/delete-icon';
 
 interface DeletePostFormProps {
-    postId: string;
-    previousUrl: string;
+  postId: string;
+  previousUrl: string;
 }
 
-export default function DeletePostForm({ postId, previousUrl }: DeletePostFormProps) {
+export default function DeletePostForm({
+  postId,
+  previousUrl,
+}: DeletePostFormProps) {
+  const [isOpen, setOpen] = useState(false);
 
-    const [isOpen, setOpen] = useState(false);
+  const [formState, action] = useFormState(
+    actions.deletePost.bind(null, postId, previousUrl),
+    { errors: {} }
+  );
 
-    const [formState, action] = useFormState(actions.deletePost.bind(null, postId, previousUrl), { errors: {} })
-
-    return <div className="flex w-full justify-end my-2">
-        <Button 
-            onClick={() => setOpen(!isOpen)}
-            type="submit"
-            color="danger"
-            variant="bordered"
-            size="sm"
-            startContent={<DeleteIcon />}
-        >
-            Delete
-        </Button>
-        <Modal isOpen={isOpen} onClose={() => setOpen(!isOpen)}>
-            <ModalContent>
-                <ModalHeader>
-
-                </ModalHeader>
-                <ModalBody>
-                    <p>Are you sure you want to delete this post?</p>
-                </ModalBody>
-                <ModalFooter>
-                    <form action={action}>
-                        <DeleteFormButton>
-                            Delete post
-                        </DeleteFormButton>
-                    </form>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
+  return (
+    <div className='flex w-full justify-end my-2'>
+      <Button
+        onClick={() => setOpen(!isOpen)}
+        type='submit'
+        color='danger'
+        variant='solid'
+        size='sm'
+        startContent={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+      <Modal isOpen={isOpen} onClose={() => setOpen(!isOpen)}>
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>
+            <p>Are you sure you want to delete this post?</p>
+          </ModalBody>
+          <ModalFooter>
+            <form action={action}>
+              <DeleteFormButton>Delete post</DeleteFormButton>
+            </form>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
+  );
 }
