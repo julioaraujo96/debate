6d66f76eb156107cf7b004ff225d2d1e20dd1cad
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-05-08 16:48:48 +0100

    fix: revalidate path fix

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,4 +1,4 @@
-'use server'
+'use server';
 
 import { Post } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
@@ -9,82 +9,81 @@ import { db } from '@/db';
 import paths from '@/paths';
 
 const createPostSchema = z.object({
-    title: z.string().min(3),
-    content: z.string().min(10),
-})
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
 
 interface CreatePostFormState {
-    errors: {
-        title?: string[],
-        content?: string[],
-        _form?: string[],
-    }
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
 }
 
 export async function createPost(
-    slug: string,
-    formState: CreatePostFormState, 
-    formData: FormData,
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
 ): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
 
-    const result = createPostSchema.safeParse({
-        title: formData.get('title'),
-        content: formData.get('content')
-    })
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
 
-    if(!result.success){
-        return {
-            errors: result.error.flatten().fieldErrors
-        }
+  const session = await auth();
+
+  if (!session || !session?.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to create a post'],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ['Cannot find topic'],
+      },
+    };
+  }
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
     }
+    return {
+      errors: {
+        _form: ['Something went wrong'],
+      },
+    };
+  }
 
-    const session = await auth();
-
-    if(!session || !session?.user ){
-        return {
-            errors: {
-                _form: ['You must be signed in to create a post']
-            }
-        }
-    }
-
-    const topic =  await db.topic.findFirst({
-            where: { slug }
-    })
-
-    if(!topic){
-        return {
-            errors: {
-                _form: ['Cannot find topic']
-            }
-        }
-    }
-    let post: Post;
-    try {
-        post = await db.post.create({
-            data: {
-                title: result.data.title,
-                content: result.data.content,
-                userId: session.user.id,
-                topicId: topic.id,
-            }
-        })
-        
-    } catch (error : unknown) {
-        if(error instanceof Error){
-            return {
-                errors: {
-                    _form: [error.message]
-                }
-            }
-        }
-        return {
-            errors: {
-                _form: ['Something went wrong']
-            }
-        }
-    }
-
-    revalidatePath(paths.topicShow(topic.slug));
-    redirect(paths.postShow(slug, post.id))
+  revalidatePath(paths.topicShow(topic.slug));
+  revalidatePath('/');
+  redirect(paths.postShow(slug, post.id));
 }
