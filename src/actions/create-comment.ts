e3e923ac45d0590c400d6c3254169bd938052407
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-05-08 17:34:33 +0100

    fix:  update homepage  when user comments  so top posts is updated

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,10 +1,10 @@
-"use server";
+'use server';
 
-import { revalidatePath } from "next/cache";
-import { z } from "zod";
-import { auth } from "@/auth";
-import { db } from "@/db";
-import paths from "@/paths";
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { db } from '@/db';
+import paths from '@/paths';
 
 const createCommentSchema = z.object({
   content: z.string().min(3),
@@ -24,7 +24,7 @@ export async function createComment(
   formData: FormData
 ): Promise<CreateCommentFormState> {
   const result = createCommentSchema.safeParse({
-    content: formData.get("content"),
+    content: formData.get('content'),
   });
 
   if (!result.success) {
@@ -37,7 +37,7 @@ export async function createComment(
   if (!session || !session.user) {
     return {
       errors: {
-        _form: ["You must sign in to do this."],
+        _form: ['You must sign in to do this.'],
       },
     };
   }
@@ -61,7 +61,7 @@ export async function createComment(
     } else {
       return {
         errors: {
-          _form: ["Something went wrong..."],
+          _form: ['Something went wrong...'],
         },
       };
     }
@@ -74,12 +74,13 @@ export async function createComment(
   if (!topic) {
     return {
       errors: {
-        _form: ["Failed to revalidate topic"],
+        _form: ['Failed to revalidate topic'],
       },
     };
   }
 
   revalidatePath(paths.postShow(topic.slug, postId));
+  revalidatePath('/');
   return {
     errors: {},
     success: true,
